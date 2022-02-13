563cba52909e9a9fe78eb670e17f46d986229e8a
copgADSI <cristiangualteros23@gmail.com>
2022-02-13 11:33:32 -0500

    Se inicializa el proyecto

---
+++ App.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { View,Text } from 'react-native'
+import Home from './components/Home'
+import Details from './components/Details'
+import Liked from './components/Liked'
+import Profile from './components/Profile'
+
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import { NavigationContainer } from '@react-navigation/native'
+
+
+const App = () => {
+  return (
+    <View>
+      <Text>  
+
+      </Text>
+    </View>
+  )
+}
+
+export default App
